781c1085adb81df571a118c5bd877dd1bf0f73ff
samfamK <skonigbgabe@byupathway.edu>
2026-01-10 13:56:18 -0500

    im done 1st design

---
+++ week01/idbage.py
@@ -0,0 +1,13 @@
+print("please enter the following information:")
+print()
+
+first_name = input("First Name:")
+last_name = input("Last Name:")
+email_address = input("Email Address:")
+phone_number = input("Phone Number:")
+job_title = input("Job Title:")
+id_number = input("Id number:")
+
+
+print(first_name)
+print(last_name)
